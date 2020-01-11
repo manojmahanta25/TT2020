c9749afcc0be729dd8a8da6cc39f208626d3472d
neelkamals36 <neelkamals36@gmail.com>
2020-01-11 21:30:32 +0530

    required files

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Mail;
+use DB;
+use App\Ticket;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MailController extends Controller {
+
+   public function html_email() {
+
+   	   $data = DB::table('tickets')->where('custid', 'TF90DYLC')->take(2)->get();
+      
+     	foreach ($data as $key) {
+     		 $custid = $key->custid;
+     		 $name = $key->name;
+			$mobile = $key->mobile;
+			$email = $key->email;
+			$pass_type = $key->pass_type;		
+			$numbers_pass = $key->numbers_pass;
+			$select_day = $key->select_day;
+			$payable_total = $key->payable_total;
+			$razor_payid = $key->razor_payid;
+			$razor_orderid = $key->razor_orderid;
+			$payment_status = $key->payment_status;		
+			$updated_at = $key->updated_at;
+     	}
+     	// echo $custid = $data->custid;
+      	$select_day=$this->getSDay($select_day);
+
+      $Mdata = [
+      	'custid'=>$custid,
+      	'name'=>$name,
+      	'mobile'=>$mobile,
+      	'email'=>$email,
+      	'pass_type'=>$pass_type,
+      	'numbers_pass'=>$numbers_pass,
+      	'select_day'=>$select_day,
+      	'payable_total'=>$payable_total,
+      	'razor_payid'=>$razor_payid,
+      	'razor_orderid'=>$razor_orderid,
+      	'payment_status'=>$payment_status
+       ];
+   	  
+   	  //$data= $data->toArray();
+   	 // echo $custid.'<br/>';
+   	   //var_dump($Mdata);
+   	  echo "<br/>";
+   	  echo "<br/>";
+   	 //print_r($Mdata);
+
+      Mail::send('mail', $Mdata, function($message) {
+         $message->to('neelkamal@kazirangauniversity.in', 'Talenttantra')->subject
+            ('Talenttantra Online Ticket receipt');
+         $message->from('noreply@talenttantra.com','Talenttantra Online Ticket');
+      });
+      echo "HTML Email Sent. Check your inbox.";
+
+   }
+
+   public function getSDay($select_day){
+        switch ($select_day) {
+           case 'first':
+                return "31st January 2020";
+                break;
+            case 'second':
+                return  "1st February 2020";
+                break;
+            case 'third':
+                return  "2nd February 2020";
+                break;
+            case 'all':
+                return  "31st January, 1st February and 2nd February 2020";
+                break;
+            default:
+                 Redirect::route('tt.ticket')->withErrors(['msg', 'Invalid Day']);
+            }
+    }
+
+
+}
---
+++ resources/views/mail.blade.php
@@ -0,0 +1,70 @@
+
+<h2>Acknowledgement</h2><br/>
+					Dear {{ $name }},<br/><br/>
+					Thank you for showing interest in Talent Tantra 2020.<br/>
+					<html><body><table style="width:100%;margin:25px;">
+                            <tbody>
+                            <tr><td style="font-size: 25px;font-weight: bold;" colspan="3">
+                            <img src="http://talenttantra.com/images/tt_250.png" alt="logo" style="float: left; margin-right: 25px;"/>Talent Tantra 2020 | Safarnama | Kaziranga University</td>
+                            <br/><br/>
+							</tr>
+                                <tr>
+
+                                    <td><strong>Payment ID : </strong>
+                                      <b>{{ $razor_payid }}</b>
+                                    </td>
+                                    <td><strong>Pass ID : </strong>
+                                      <b>{{ $custid }}</b>
+                                    </td>
+                                    <td><strong>Pass Type : </strong>
+                                      {{ $pass_type }} pass
+                                    </td>
+                                    
+                                </tr>
+                                <tr>
+                                	<td><strong>Quantity : </strong>
+                                      {{ $numbers_pass }}
+                                    </td>
+									<td><strong>Booked Date for : </strong>
+                                       {{ $select_day }}
+                                    </td>
+                                    <td><strong>Name : </strong>
+                                       {{ $name }}
+                                    </td>                                    
+                                </tr>
+                                <tr>
+                                	<td><strong>Email : </strong>
+                                        {{ $email }}
+                                    </td>
+                                    <td><strong>Mobile : </strong>
+                                       {{ $mobile }}
+                                    </td>
+                                    <td><strong>Paid : </strong>
+                                        {{ $payable_total }}
+                                    </td>
+                                </tr>
+                                
+								<tr>
+								<td colspan="3"></td>
+								</tr>
+								<tr>								
+								<td colspan="3">
+								<strong>Terms & Conditions:</strong>
+								<br>
+								 <ul> 
+									<li>This pass is valid for <strong>{{ $select_day }}</strong>.</li>
+					                <li>Entry into the campus is from <strong>9 a.m.</strong> onwards.</li>
+					                <li>Entry without passes will not be permitted. Please carry your pass to the venue. The same should be produced upon request at the gate.</li>
+					                <li>Organizers reserve the right to perform <strong>security checks</strong> in the members/invitees at the entry point.</li>
+					                <li>Parking area is available outside the University premises, and is at the risk of the vehicle owner. Organizers will not hold responsibility for any damage or theft of the vehicles.</li>
+					                <li>Consumption or sale of <strong>alcohol</strong>,<strong> tobacco</strong> or <strong>being intoxicated/under the influence of narcotics</strong> is strictly prohibited on campus. This is a<strong> smoke-free zone</strong> and the organisers reserve the right to <strong>expel violators</strong> from the campus.</li>
+					                <li>A pass shall not be valid if the <strong>barcode/serial number</strong> has been tampered with. <strong>Unlawful resale of ticket</strong> would lead to seizure or cancellation of that pass without refund or compensation.</li>
+					                <li><strong>Duplicate tickets</strong> will not be issued for lost or stolen tickets. No refund for a purchased ticket is possible.</li>
+					                <li>In case of cancellation of the shows due to any unforseen circumstances beyond the control of the organizers, the extent of the refurnd will be based on the situation at hand and the management's decision will be final and binding will be final and binding on all.</li>
+								</ul>
+								</td>
+								</tr>
+                            </tbody>
+                        </table></body></html><br/>
+					In case for any information please contact us @ <b>8486034362 | 6000903183</b> or email us at <b>talenttantra@kazirangauniversity.in</b><br/><br/>
+					Thank you and best regards,<br/><br/>Team TalentTantra<br/><br/>
