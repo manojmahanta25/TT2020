0bc8737217981f84f97063a85c89025267f9b0b6
neelkamals36 <neelkamals36@gmail.com>
2020-01-12 16:51:42 +0530

    un

---
+++ resources/views/evt/treasurehunt.blade.php
@@ -0,0 +1,81 @@
+<section id="portfolio" class="portfolio" style="background: url(../images/events/evnt_bck.jpg)center center no-repeat;background-size:cover; ">
+    <div class="container">
+
+        <div class="heading" style="padding-top:50px;">
+            <h2>Tresure Hunt</h2>
+        </div>
+        <!--end heading-->
+
+        <div class="portfoliomain">
+            <div class="col-md-7 col-sm-12 col-xs-12" style="background:rgba(255,253,253,0.8);padding: 12px;">
+                <div class="speaker-content">
+                    <span><h3>What&#39;s your best price? It&#39;s the Treasure Hunt that makes it fun.</h3></span>
+                    <p>A treasure hunt is one of many different types of games for players who try to find hidden objects or places by following a series of clues. There will be 2 members in each team. They will get some clues that will lead to the next clue and after getting all the clues the team will be able to find the treasure. The winner will get a prize money.</p>
+                </div>
+                <br/><br/><br/>
+            </div>
+
+            <div class="col-md-5 col-sm-12 col-xs-12">
+                <div class="speaker-image" style="border-image: url(../images/events/stamp-border.png) 8 round;border-width: 8px;border-style: solid;border-image-outset: 1;background: url(../images/events/informal.svg) center center no-repeat , rgba(48,185,229,0.8);background-size: 50%;height:20em;">
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-12" style="margin-top: 80px; float: left;">
+            <section class="schedules">
+                <div class="container">
+                    <div class="schedule-tabs">
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="event-one">
+                                <div class="schedule-table table-responsive">
+                                    <div class="speaker-statement" aria-hidden="true">
+                                        <h5 style="text-align: justify;">Rules And Regulations:</h5>
+                                        <ol style="text-align: justify; list-style-type: decimal; margin-left: 50px; margin-top: 10px;">
+                                            <li>One team consisting of two members.</li>
+                                            <li>Time will be limited based on the plot.</li>
+                                            <li>The treasure hunt consists of some clues, each one leading to the next clue.</li>
+                                            <li>The clues are to be found in a particular order. A team cannot skip a clue. Skipping a clue leads to disqualification of the team.</li>
+                                            <li>Do not search for clues in off-limit areas.</li>
+                                            <li>If you found other team&#39;s clue by any chance never tamper it. A violation of this  rule could cause disqualification of your team.</li>
+                                            <li>All the classes of the teams are different and located at different places but the final destination i.e. the treasure is same. The team which finds the treasure wins first</li>
+                                        </ol><br/>
+                                        <div class="speaker-statement">
+                                            <h5 style="text-align: justify;">Event Schedule:</h5>
+                                            <ul style="text-align: justify; list-style:none;">
+                                                <li>Venue: Whole campus</li>
+                                                <li>Starting Time: xxxxxxxxxxxxxxxxxx</li>
+                                                <li>Date: 2<sup>nd</sup> February, 2020</li>
+                                            </ul>
+                                        </div><br/>
+                                        <div class="speaker-address">
+                                            <ul style="text-align: justify; list-style:none;">
+                                                <li><strong>Event Coordinator:</strong></li>
+                                                <li><span><i class="fa fa-phone" aria-hidden="true"></i></span> Bedanta Saikia</li>
+                                                <li><span><i class="fa fa-phone" aria-hidden="true"></i></span> Himanta Gogoi +91 80118 60068</li>
+{{--                                                <li><span><i class="fa fa-envelope-o" aria-hidden="true"></i></span>Bivash Ghosh</li>--}}
+                                            </ul>
+                                        </div>
+                                        <div class="speaker-statement">
+                                            <h5 style="text-align: justify;">Registration Fee:</h5>
+                                            <p style="text-align: justify;"><strong> Rs. 500 /MEMBERS</strong></p><br/>
+{{--                                            <form action="registration.php" method="GET" style="float:left;">--}}
+{{--                                                <input type="hidden" name="event" value="fal-solo">--}}
+{{--                                                <input type="submit" name="eventregistrationbt" value="register now" style="text-transform: uppercase;font-size: 14px; padding: 18px 24px;">--}}
+{{--                                            </form>--}}
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- schedule-table -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+                <!-- container -->
+    </div>
+        <!--end project-grid-->
+    <!--end portfoliomain-->
+    <!--end container-->
+</section>
