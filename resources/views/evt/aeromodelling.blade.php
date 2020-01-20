7796d01013d464bdb2d3272f9614db0999d6c95b
neelkamals36 <neelkamals36@gmail.com>
2020-01-20 11:51:29 +0530

    are

---
+++ resources/views/evt/aeromodelling.blade.php
@@ -0,0 +1,85 @@
+<section id="portfolio" class="portfolio" style="background: url(../images/events/evnt_bck.jpg)center center no-repeat;background-size:cover; ">
+    <div class="container">
+
+        <div class="heading" style="padding-top:50px;">
+            <h2>AERO - MODELLING</h2>
+        </div>
+        <!--end heading-->
+
+        <div class="portfoliomain">
+            <div class="col-md-7 col-sm-12 col-xs-12" style="background:rgba(255,253,253,0.8);padding: 12px;">
+                <div class="speaker-content">
+                    <span><h3>A Science and Technology Event</h3></span>
+                    <p>Aeromodelling is going to be high energy event in which the students will be tested based on their creativity and understanding to apply the fundamentals of physics and aerodynamics along with manufacturing bent of mind</p>
+                                       <br/><br/><br/><br/><br/><br/><br/>
+                </div>
+            </div>
+
+            <div class="col-md-5 col-sm-12 col-xs-12">
+                <div class="speaker-image" style="border-image: url(../images/events/stamp-border.png) 8 round;border-width: 8px;border-style: solid;border-image-outset: 1;background: url(../images/events/science.svg) center center no-repeat , rgba(48,185,229,0.8);background-size: 50%;height:20em;">
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-12" style="margin-top: 80px; float: left;">
+            <section class="schedules">
+                <div class="container">
+                    <div class="schedule-tabs">
+                        <!-- Nav tabs -->
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="event-one">
+                                <div class="schedule-table table-responsive">
+                                    <div class="speaker-statement" aria-hidden="true">
+                                        <h4 style="text-align: justify;color: lightcoral">Aero - Modelling</h4>
+                                        <h5 style="text-align: justify;">Rules And Regulations:</h5>
+                                        <ul style="text-align: justify; list-style-type: disc; margin-left: 30px; margin-top: 10px;">
+                                            <li>Students must bring their own flying objects</li>
+                                            <li>The flying object should not be more than 60×60cm (±10cm)</li>
+                                            <li>Three Shots will be given to each participant  best of three will count .</li>
+                                            <li>The bottom of the flying object should be of hard material to hook it with string</li>
+                                            <li>Each team can have upto two participants</li>
+                                            <li>Entry Fees will be 500 per participant, for a team of two the entry fees will be 1000</li>
+
+                                        </ul><br/>
+                                        <!-- <div class="speaker-statement">
+                                            <h5 style="text-align: justify;">Event Schedule:</h5>
+                                            <ul style="text-align: justify; list-style:none;">
+                                                <li>Venue: xxxxxxxxxxxxxxxxxx</li>
+                                                <li>Starting Time: xxxxxxxxxxxxxxxxxx</li>
+                                                <li>Date: xxxxxxxxxxxxxxxxxxxxxxxxxxxx</li>
+                                            </ul>
+                                        </div><br/> -->
+                                        <div class="speaker-statement">
+                                            <ul class="speaker-address" style="text-align: justify;">
+                                                <h5>Event Coordinator:</h5>
+                                                <li><span><i class="fa fa-phone" aria-hidden="true"></i></span>Ujjwal Jha: 7002238952 / 8011590609</li>
+                                                <li><span><i class="fa fa-phone" aria-hidden="true"></i></span>Harpreet Singh: 6002782430</li>
+                                            </ul>
+                                        </div><br/>
+                                        <div class="speaker-statement">
+                                            <h5 style="text-align: justify;">Registration Fee:</h5>
+                                            <p style="text-align: justify;"><strong> Rs. 500 /MEMBERS</strong></p><br/>
+                                        </div>
+                                        <div class="speaker-statement" style="text-align: left;margin-left: 20px;">
+                                            <a class="ristrabtn" href="{{route('tt.registersingle','aeromodelling')}}" alt="">REGISTER</a>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- schedule-table -->
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </div>
+                <!-- container -->
+    </div>
+        <!--end project-grid-->
+    <!--end portfoliomain-->
+    <!--end container-->
+</section>
