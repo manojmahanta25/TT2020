e21259fcff087b414124214d2b27717ffd310251
neelkamals36 <neelkamals36@gmail.com>
2020-01-29 12:31:29 +0530

    csr

--- resources/views/csr.blade.php
+++ resources/views/csr.blade.php
@@ -3,11 +3,22 @@
     <div class="container">
 
         <div class="heading" style="padding-top:50px;">
-            <span>Meet Our</span>
-            <h2>Team</h2>
+            <span>Corporate social responsibility (CSR)</span>
         </div>  
 
-</div>
+	</div>
+	<div class="container">
+		<div class="col-md-12">
+			<iframe width="100%" height="500" src="https://www.youtube.com/embed/rUPG7RnZPj0?autoplay=1&controls=0&rel=0&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div>
+		<div class="col-md-12" style="padding:10px;">
+			<p style="text-align: justify;">In a world that is constantly changing, take some time out to think about the lives that children suffering from hunger and cancer lead. Help these children lead a healthy life by giving them the gift of nutrition. This Talent Tantra, Buy a Pass and contribute towards a noble cause. It takes every drop to make an ocean, be that drop! Every little contribution counts.</p>
+			<h5 style="margin-top:10px;">Food Drives in Collaboration</h5>
+			<p style="font-weight: 900;">
+			<img src="{{ URL::asset('images/sponsors/ZFI_JorhatChapter.jpg') }}" alt="Zomato Feeding India (Jorhat Chapter)" style="background-color: white; border-radius: 10px; margin-left: 10px;"/>
+			</p>
+		</div>
+	</div>
 </section>
 @include('elements.footerwidget')
 @include('elements.footer')
